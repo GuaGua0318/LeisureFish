224849802f4102b7a364189e8dbfcfcfed85aa87
GuaGua0318 <15972660547@163.com>
2023-12-28 23:13:21 +0800

    feat:首页

--- src/renderer/src/views/Home/index.tsx
+++ src/renderer/src/views/Home/index.tsx
@@ -1,9 +1,63 @@
+import { Card, Col, Row } from 'antd'
 import React from 'react'
+import styles from './home.module.scss'
 
 type Props = {}
 
-export default function Home({}: Props) {
-  return (
-    <div>Home</div>
-  )
+export default function Home({ }: Props) {
+    return (
+        <div>
+            <Row gutter={16}>
+                <Col span={8}>
+                    <Card title="Card title" bordered={false} className={styles.test}>
+                        Card content
+                    </Card>
+                </Col>
+                <Col span={8}>
+                    <Card title="Card title" bordered={false}>
+                        Card content
+                    </Card>
+                </Col>
+                <Col span={8}>
+                    <Card title="Card title" bordered={false}>
+                        Card content
+                    </Card>
+                </Col>
+            </Row>
+            <Row gutter={16}>
+                <Col span={8}>
+                    <Card title="Card title" bordered={false}>
+                        Card content
+                    </Card>
+                </Col>
+                <Col span={8}>
+                    <Card title="Card title" bordered={false}>
+                        Card content
+                    </Card>
+                </Col>
+                <Col span={8}>
+                    <Card title="Card title" bordered={false}>
+                        Card content
+                    </Card>
+                </Col>
+            </Row>
+            <Row gutter={16}>
+                <Col span={8}>
+                    <Card title="Card title" bordered={false}>
+                        Card content
+                    </Card>
+                </Col>
+                <Col span={8}>
+                    <Card title="Card title" bordered={false}>
+                        Card content
+                    </Card>
+                </Col>
+                <Col span={8}>
+                    <Card title="Card title" bordered={false}>
+                        Card content
+                    </Card>
+                </Col>
+            </Row>
+        </div>
+    )
 }
