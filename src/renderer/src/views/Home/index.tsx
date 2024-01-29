28c7430ce12e03e26ca2b995e134c47d2b00d5af
GuaGua0318 <15972660547@163.com>
2024-01-29 22:29:46 +0800

    feat:吃什么组件

--- src/renderer/src/views/Home/index.tsx
+++ src/renderer/src/views/Home/index.tsx
@@ -6,6 +6,7 @@ import styles from './home.module.scss'
 import { useNavigate } from 'react-router'
 import { ArrowRightOutlined } from '@ant-design/icons'
 import { LuckyWheel, LuckyGrid } from 'react-luck-draw'
+import 'wc-waterfall'
 
 const imgg = 'https://img0.baidu.com/it/u=146494919,1943694376&fm=253&fmt=auto&app=138&f=JPEG?w=5&h=5'
 
@@ -53,14 +54,15 @@ const Home = () => {
   const navigate = useNavigate()
   const myLuckey = useRef(null)
   const [eatSetting, setEatSetting] = useState<boolean>(false)
-  const [selectCheckbox,setSelectCheckbox] = useState([1,2])
+  const [selectCheckbox,setSelectCheckbox] = useState([1,2,3,4,5,6,7,8])
 
   return (
     <>
       <UserLoginHd />
       <DrinkRemind />
+      
       <Row gutter={16}>
-        <Col span={8}>
+        <Col span={6}>
           <Card title="取色器" bordered={false}>
             <Space size={100}>
               <ColorPicker size="small" showText />
@@ -73,7 +75,7 @@ const Home = () => {
             </Space>
           </Card>
         </Col>
-        <Col span={8}>
+        <Col span={10}>
           <Card title="吃什么" bordered={false} extra={
             <a
               href="#"
@@ -86,9 +88,10 @@ const Home = () => {
           }>
             <LuckyWheel width="300px" height="300px" ref={myLuckey} blocks={blocks}
               prizes={selectCheckbox && selectCheckbox.map((item) => {
-                return  { title: '1元红包', background: '#f9e3bb', fonts: [{ text: '1元红包', top: '18%' }],imgs:[{
+                return  { title: '1元红包', background: '#f9e3bb',imgs:[{
                   src: 'https://img0.baidu.com/it/u=146494919,1943694376&fm=253&fmt=auto&app=138&f=JPEG?w=5&h=5',
                   width: '40%',
+                  height:'50%',
                   top: '10%'
                 }] }
               })}
