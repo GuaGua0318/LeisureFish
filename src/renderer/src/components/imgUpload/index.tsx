eb8f3cd126c8bc80b39e7afd084247f75482cae7
GuaGua0318 <15972660547@163.com>
2024-02-05 17:16:54 +0800

    feat:点击图片获取色号

--- src/renderer/src/components/imgUpload/index.tsx
+++ src/renderer/src/components/imgUpload/index.tsx
@@ -3,6 +3,23 @@ import styles from './imgUpload.module.scss'
 import { PlusOutlined } from '@ant-design/icons'
 import { Col, Row } from 'antd'
 
+type randomcolorFn = () => string
+type colorstyle = {
+  height: string
+  fontSize: string
+  textAlign: string
+  lineHeight: string
+}
+const randomColor: randomcolorFn = () => {
+  return '#' + Math.random().toString(16).substr(2, 6)
+}
+const colorStyle: colorstyle = {
+  height: '70px',
+  fontSize: '22px',
+  textAlign: 'center',
+  lineHeight: '70px'
+}
+
 const ImgUpload = () => {
   const [imgUrl, setImgUrl] = useState()
   const [color, setColor] = useState<string>()
@@ -55,12 +72,41 @@ const ImgUpload = () => {
         ) : null}
       </div>
       <Row>
-        <Col span={6} style={{ height: '70px', background: `${color}`,fontSize:'22px',textAlign:'center',lineHeight:'70px' }}>
+        <Col
+          span={6}
+          style={{
+            height: '70px',
+            background: `${color}`,
+            fontSize: '22px',
+            textAlign: 'center',
+            lineHeight: '70px'
+          }}
+        >
           {color}
         </Col>
-        <Col span={6} style={{ height: '70px', background: 'pink' }}></Col>
-        <Col span={6} style={{ height: '70px', background: 'greeb' }}></Col>
-        <Col span={6} style={{ height: '70px', background: 'yellow' }}></Col>
+        <Col span={6} style={{ ...colorStyle, background: `${randomColor()}` }}>
+          {randomColor()}
+        </Col>
+        <Col span={6} style={{ ...colorStyle, background: `${randomColor()}` }}>
+          {randomColor()}
+        </Col>
+        <Col span={6} style={{ ...colorStyle, background: `${randomColor()}` }}>
+          {randomColor()}
+        </Col>
+      </Row>
+      <Row>
+        <Col span={6} style={{ ...colorStyle, background: `${randomColor()}` }}>
+          {randomColor()}
+        </Col>
+        <Col span={6} style={{ ...colorStyle, background: `${randomColor()}` }}>
+          {randomColor()}
+        </Col>
+        <Col span={6} style={{ ...colorStyle, background: `${randomColor()}` }}>
+          {randomColor()}
+        </Col>
+        <Col span={6} style={{ ...colorStyle, background: `${randomColor()}` }}>
+          {randomColor()}
+        </Col>
       </Row>
     </>
   )
